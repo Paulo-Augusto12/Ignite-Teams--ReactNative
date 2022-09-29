895e33195c18824b227e975b529f4ee0a7d2cccc
Paulo Augusto <paulo-augusto12@outlook.com>
2022-09-29 10:00:20 -0300

    Inicio da estilização - Styled Components

--- igniteTeams/src/screens/Groups/index.tsx
+++ igniteTeams/src/screens/Groups/index.tsx
@@ -1,24 +1,23 @@
-import { StyleSheet , Text, View } from 'react-native'
+import { StyleSheet , Text, View, Image } from 'react-native'
+
+import { 
+
+    Title, 
+    Container 
+
+} from './style'
 
 
 export function Groups(){
 
     return(
-        <View style={styles.container}>
-        <Text>
-            Groups
-        </Text>
-        </View>
+        <Container>
+            <Title>        
+                Groups
+            </Title>
+        </Container>
     )
 
 }
 
-const styles = StyleSheet.create({
-    container: {
-      flex:1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
   
---
+++ igniteTeams/src/screens/Groups/style.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components/native'
+
+
+export const Container = styled.View`
+
+    flex:1;
+    background-color: #000;
+    align-items: center;
+    justify-content: center;
+
+`
+
+export const Title = styled.Text`
+
+    color:#fff;
+    font-size: 34px;
+    font-weight: 600;
+        
+`
+
