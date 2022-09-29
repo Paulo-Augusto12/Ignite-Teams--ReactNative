d1ab32cbc469b02d01a1250c0e7f2bc2b1e222e7
Paulo Augusto <paulo-augusto12@outlook.com>
2022-09-29 09:07:04 -0300

    configurando path mapping

--- igniteTeams/App.tsx
+++ igniteTeams/App.tsx
@@ -1,20 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { Groups } from '@screens/Groups';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+ return(
+  <Groups />
+ )
+
+}
